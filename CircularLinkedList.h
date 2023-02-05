3d07386c5a05d5269e9498fb88b73cbdad103c1c
Enqute <61355266+Enqute@users.noreply.github.com>
2023-02-05 06:11:16 -0800

    Circular Linked updated
    The Circular Linked List is done

--- CircularLinkedList.h
+++ CircularLinkedList.h
@@ -1,136 +1,213 @@
-#pragma once
-
 #include <iostream>
-#include <cstdlib>
+#include <stdlib.h>
 #include <string>
 
-#define ASSERT(x, message) if (!(x)) { std::cout << message << std::endl; std::system("pause"); exit(-1); } 
+#define ASSERT(x, message) if (!(x)) { std::cout << message << std::endl; system("Pause"); exit(-1); }
 
 template<typename T>
 class CircularLinkedList
 {
 private:
-    struct Node
-    {
-        T Data;
-        Node* Next;
-        Node* Prev;
-    };
-    
+	struct Node
+	{
+		T Data;
+		Node* Prev;
+		Node* Next;
+	};
+
 private:
-    Node* m_Head = nullptr;
-    Node* m_Tail = nullptr;
-    size_t m_Size = 0;
-    
+	#define null NULL;
+	size_t m_Size = 0;
+	Node* m_Head = nullptr;
+	Node* m_Tail = nullptr;
+
 public:
-    inline size_t Size() const
-    {
-        return m_Size;
-    }
-    
-    bool IsEmpty() const
-    {
-        return m_Size == 0;
-    }
+	const size_t Size() const
+	{
+		return m_Size;
+	}
 
-    int IndexOf(const T& item) const
+	const bool IsEmpty() const
+	{
+		return m_Size == 0;
+	}
+
+	const int IndexOf(const T& item) const
+	{
+		ASSERT(!IsEmpty(), "[Error]: IllegalAccessException thrown from 'IndexOf()'.");
+		
+		size_t index = 0;
+		Node* node = m_Head;
+
+		for (; node != m_Tail; node = node->Next)
+		{
+			if (node->Data == item)
+				return index;
+			index++;
+		}
+
+		return -1;
+	}
+
+	const bool Contains(const T& item) const
+	{
+		return IndexOf(item) != -1;
+	}
+
+	void Add(const T& item)
+	{
+		AddLast(item);
+	}
+
+	void AddFirst(const T& item)
+	{
+		Node* node = new Node{ item, nullptr, nullptr };
+		if (IsEmpty())
+			m_Head = m_Tail = node;
+		else
+		{
+			node->Next = m_Head;
+			m_Head->Prev = node;
+			node->Prev = m_Tail;
+			m_Head = node;
+		}
+		m_Size++;
+	}
+
+	void AddLast(const T& item)
+	{
+		Node* node = new Node{ item, nullptr, nullptr };
+		if (IsEmpty())
+			m_Head = m_Tail = node;
+		else
+		{
+			m_Tail->Next = node;
+			node->Prev = m_Tail;
+			node->Next = m_Head;
+			m_Tail = node;
+		}
+		m_Size++;
+	}
+
+	const T& PeekFirst() const
+	{
+		ASSERT(!IsEmpty(), "[Error]: IllegalAccessException thrown from 'PeekFirst()'.");
+		return m_Head->Data;
+	}
+
+	const T& PeekLast() const
+	{
+		ASSERT(!IsEmpty(), "[Error]: IllegalAccessException thrown from 'PeekFirst()'.");
+		return m_Tail->Data;
+	}
+
+	T& RemoveFirst()
+	{
+		ASSERT(!IsEmpty(), "[Error]: IllegalAccessException thrown from 'RemoveFirst()'.");
+		T data = m_Head->Data;
+		if (m_Head == m_Tail)
+		{
+			m_Head->Data = m_Tail->Data = null;
+			delete m_Head->Next, m_Head->Prev, m_Head;
+			delete m_Tail->Next, m_Tail->Prev, m_Tail;
+		}
+		else
+		{
+			Node* node = m_Head->Next;
+			delete m_Head;
+			node->Prev = m_Tail;
+			m_Head = node;
+		}
+		m_Size--;
+		return data;
+	}
+
+	T& RemoveLast()
+	{
+		ASSERT(!IsEmpty(), "[Error]: IllegalAccessException thrown from 'RemoveLast()'.");
+		T data = m_Tail->Data;
+		if (m_Head == m_Tail)
+		{
+			m_Head->Data = m_Tail->Data = null;
+			delete m_Head->Next, m_Head->Prev, m_Head;
+			delete m_Tail->Next, m_Tail->Prev, m_Tail;
+		}
+		else
+		{
+			Node* node = m_Tail->Prev;
+			delete m_Tail;
+			node->Next = m_Head;
+			m_Tail = node;
+		}
+		m_Size--;
+		return data;
+	}
+
+	T& Remove(const T& item)
+	{
+		ASSERT(!IsEmpty(), "[Error]: IllegalAccessException thrown from 'Remove(...)'.");
+		ASSERT(Contains(item), "[Error]: IllegalArgumentException thrown from 'Remove(...)'.");
+
+		if (item == m_Head->Data)
+			return RemoveFirst();
+		if (item == m_Tail->Data)
+			return RemoveLast();
+
+		T data = item;
+
+		Node* node = m_Head;
+		for (; node->Data != item; node = node->Next);
+		Node* temp = node;
+		node->Prev->Next = node->Next;
+		node->Next->Prev = node->Prev;
+		node->Data = temp->Data = null;
+		delete temp;
+		m_Size--;
+
+		return data;
+	}
+
+	T& RemoveAt(const size_t& index)
+	{
+		ASSERT(!IsEmpty(), "[Error]: IllegalAccessException thrown from 'Remove(...)'.");
+		ASSERT(index >= 0 && index < m_Size, "[Error]: IllegalArgumentException thrown from 'RemoveAt(...)'.");
+
+		if (index == 0)
+			return RemoveFirst();
+		if (index == m_Size - 1)
+			return RemoveLast();
+
+		size_t counter = 0;
+		Node* node = m_Head;
+		for (; (node != m_Tail && counter != index); node = node->Next)
+			counter++;
+		Node* temp = node;
+		T data = node->Data;
+		node->Prev->Next = node->Next;
+		node->Next->Prev = node->Prev;
+		node->Data = temp->Data = null;
+		delete temp;
+		m_Size--;
+
+		return data;
+	}
+	
+	const std::string ToString() const
     {
-        ASSERT(!IsEmpty(), "[Error]: IllegalAccessException is thrown from 'IndexOf(...)'. You are trying to access elements of empty list!");
-        int counter = 0;
-        for (Node* node = m_Head; node != nullptr; node = node->Next)
+        std::string string = "[";
+        bool iterate = true;
+        for (Node* node = m_Head; iterate; node = node->Next)
         {
-            if (node->Data == item)
-                return counter;
-            counter++;
+            std::string right = node == m_Tail ? "]" : ", ";
+            string += std::to_string(node->Data) + right;
+            if (node == m_Tail)
+                iterate = false;
         }
-        return -1;
+        return string;
     }
 
-    bool Contains(const T& item) const
+    void Print() const
     {
-        return IndexOf(item) != -1;
-    }
-
-    void Add(const T& item)
-    {
-        AddLast(item);
-    }
-
-    void AddLast(const T& item)
-    {
-        Node* node = new Node{ item, nullptr, nullptr };
-        if (IsEmpty())
-            m_Head = m_Tail = node;
-        else
-        {
-            m_Tail->Next = node;
-            node->Prev = m_Tail;
-            m_Tail = node;
-        }
-        m_Head->Prev = m_Tail;
-        m_Tail->Next = m_Head;
-        m_Size++;
-    }
-
-    void AddFirst(const T& item)
-    {
-        Node* node = new Node{ item, nullptr, nullptr };
-        if (IsEmpty())
-            m_Head = m_Tail = node;
-        else
-        {
-            node->Next = m_Head;
-            m_Head->Prev = node;
-            m_Head = node;
-        }
-        m_Head->Prev = m_Tail;
-        m_Tail->Next = m_Head;
-        m_Size++;
-    }
-
-    T& PeekFirst() const
-    {
-        ASSERT(!IsEmpty(), "[Error]: IllegalAccessException is thrown from 'PeekFirst()'. You are trying to access elements of empty list!");
-        return m_Head->Data;
-    }
-
-    T& PeekLast() const
-    {
-        ASSERT(!IsEmpty(), "[Error]: IllegalAccessException is thrown from 'PeekLast()'. You are trying to access elements of empty list!");
-        return m_Tail->Data;
-    }
-
-    T& RemoveFirst()
-    {
-        ASSERT(!IsEmpty(), "[Error]: IllegalAccessException is thrown from 'RemoveFirst()'. You are trying to remove elements of empty list!");
-        if (m_Head == m_Tail)
-        {
-            delete m_Head, m_Tail;
-            m_Head = m_Tail = nullptr;
-        }
-        else
-        {
-            m_Tail->Next = m_Head->Next;
-            m_Head->Next->Prev = m_Tail;
-            m_Head = m_Head->Next;
-        }
-        m_Size--;
-    }
-
-    T& RemoveLast()
-    {
-        ASSERT(!IsEmpty(), "[Error]: IllegalAccessException is thrown from 'RemoveLast()'. You are trying to remove elements of empty list!");
-        if (m_Head == m_Tail)
-        {
-            delete m_Head, m_Tail;
-            m_Head = m_Tail = nullptr;
-        }
-        else
-        {
-            m_Head->Prev = m_Tail->Prev;
-            m_Tail->Prev->Next = m_Head;
-            m_Tail = m_Tail->Prev;
-        }
-        m_Size--;
+        std::cout << ToString() << std::endl;
     }
 };
