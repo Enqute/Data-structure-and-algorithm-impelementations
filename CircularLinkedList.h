60f1bf867cc82739807015afd026a40363dcc412
Enqute <61355266+Enqute@users.noreply.github.com>
2023-02-10 10:35:55 -0800

    Insert added to Circular Linked List

--- CircularLinkedList.h
+++ CircularLinkedList.h
@@ -88,6 +88,28 @@ public:
 		}
 		m_Size++;
 	}
+	
+	void Insert(const T& item, int index)
+	{
+	    ASSERT(index >= 0 && index <= m_Size, "[Error]: IndexOutOfBoundException thrown from 'Insert(...)'.");
+	    if (index == 0)
+	        AddFirst(item);
+	    else if (index == m_Size)
+	        AddLast(item);
+	    else
+	    {
+	        Node* elem = new Node{ item, nullptr, nullptr };
+	        Node* node = m_Head;
+	        int counter = 0;
+	        for (; (node != m_Tail && counter != index - 1); node = node->Next, counter++);
+	        Node* temp = node->Next;
+	        node->Next = elem;
+	        elem->Prev = node;
+	        elem->Next = temp;
+	        temp->Prev = elem;
+	        m_Size++;
+	    }
+	}
 
 	const T& PeekFirst() const
 	{
@@ -211,3 +233,17 @@ public:
         std::cout << ToString() << std::endl;
     }
 };
+
+int main()
+{
+    CircularLinkedList<int> numbers;
+    numbers.Add(1);
+    numbers.Add(2);
+    numbers.Add(3);
+    numbers.Add(4);
+    numbers.Insert(12, 4);
+    numbers.Insert(120, 1);
+    
+    numbers.Print();
+    std::cin.get();
+}
